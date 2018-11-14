9eb65156690b6e8423c2eadf9d6302ac0a28edd4
VidhyaVadivel <33505767+VidhyaVadivel@users.noreply.github.com>
2018-11-14 15:10:19 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+	 int[][] mat = new int[10][10];
+	 int x=1;
+        mat[(Integer.parseInt(args[1]))-x][(Integer.parseInt(args[2]))-x]=Integer.parseInt(args[0]);
+        for(int i=0;i<10;i++)
+        {
+            for(int j=0;j<10;j++)
+            {
+                System.out.print(mat[i][j]);
+            }
+            System.out.println();
+        }
+        
+	}
+}
