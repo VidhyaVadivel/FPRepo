b91c9467a88d1c39bdb819220c1b6da9b67d7ef9
VidhyaVadivel <33505767+VidhyaVadivel@users.noreply.github.com>
2018-11-14 15:14:09 +0530

    Add files via upload

--- Main (1).java
+++ Main (1).java
@@ -9,18 +9,12 @@ Write your code in this editor and press "Run" button to execute it.
 public class Main
 {
 	public static void main(String[] args) {
+	 int res=1;
+	    for(int i=Integer.parseInt(args[0]);i>0;i--)
+	    {
+	        res=res*i;
+	    }
+	    System.out.println("Factorised value:"+res);
 		
-	 int[][] mat = new int[10][10];
-	 int x=1;
-        mat[(Integer.parseInt(args[1]))-x][(Integer.parseInt(args[2]))-x]=Integer.parseInt(args[0]);
-        for(int i=0;i<10;i++)
-        {
-            for(int j=0;j<10;j++)
-            {
-                System.out.print(mat[i][j]);
-            }
-            System.out.println();
-        }
-        
 	}
 }
